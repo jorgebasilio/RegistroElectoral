33fe85b952edfa51543cc4a05cdb7e0c1eb0bf0b
Ruben Luna <rubenlunamate@gmail.com>
2015-12-16 09:23:45 -0430

    Update Register.h

--- Register.h
+++ Register.h
@@ -56,7 +56,7 @@ void addRegister(Register **r, char name[65], char address[100], char number[12]
 				t = t->next;
 			t->next = aux;
 		}
-		else printf("Registro Estatal ya registrado!!");
+		else printf("Codigo Registro Estatal ya registrado!!");
 	}
 }
 
@@ -73,7 +73,7 @@ void addCenterRegister(Register *r, char codeR[4])
 				scanf("%s", &codeC);
 				scanf("%s", &name);
 				scanf("%s", &address);
-				addCenter(&(r)->down, name, address, codeC);
+				addCenter(&(r)->down, name, address, codeC, codeR);
 				break;
 			}
 			r = r->next;
@@ -81,7 +81,7 @@ void addCenterRegister(Register *r, char codeR[4])
 	}
 }
 
-void seachCenterRegister(Register *r, char codeC[8])
+void seachCenterRegister(Register *r, char codeC[4])
 {
 	bool flag = false;
 	if (!vacio(r))
@@ -112,7 +112,7 @@ void seeAll(Register *r)
 	{
 		while (r)
 		{
-			printf("\n\n Caja [%i] Registro : \n\n", cont);
+			printf("\n\n Caja [%i] Registro Estatal: \n\n", cont);
 			seeRegister(r);
 			printf("\n\n");
 			seePersonCenter(r->down);
@@ -124,17 +124,142 @@ void seeAll(Register *r)
 	}
 }
 
-void seachDeleteCenter(Register **r, char codeC[8], bool flag) // busca el la caja principal a eliminar
+void seachDeleteCenter(Register **r, char code[8], bool flag) // busca el la caja principal a eliminar
 {
-	Register *aux = (*r);
-	if (!(vacio(aux)))
+	Register *t = (*r);
+	if (!(vacio(t)))
 	{
-		while (aux)
+		while (t)
 		{
 			printf("\n Entro while seachDeleteCenter");
-			seachChangeDelete(&(aux)->down, codeC, flag);
-			aux = aux->next;
+			Center *aux = (*r)->down;
+			if ((strcmp(code, aux->code) == 0)) //caso cabeza
+			{
+				(*r)->down = (aux)->next;
+				if (flag)
+					DeletePeople(&(aux)->left, &aux);
+				else // else if (!flag && !vacio(t->down))
+					changeCenter(&aux, &(aux)->left, code);
+				delete aux;
+				break;
+			}
+			else
+				seachChangeDeleteCenter(&(t)->down, code, flag);
+			t = t->next;
 		}
 	}
 } // falta el caso de si es el cabeza de a lista. 
 
+void deleteCenterPeople(Center **c) 
+{}
+
+void changeRegister(Register **r, Center **c, char codeR[4]) // parametros: lista principal completa, sub-lita a enlazar, numero X que se va a eliminar de la principal 
+{
+	char codeRNew[4];
+	printf("\n Codigo Region Estatal donde desea mover los centros :");
+	scanf("%s", &codeRNew);
+	Register *aux = (*r);
+	while (aux)
+	{
+		printf("\n Entro while changeRegister :");
+		if ((strcmp(codeRNew, aux->code) == 0) && (strcmp(codeRNew, codeR) != 0)) // se busca la caja principal a enlazar con sublista de X, mientras sea distinto de X
+		{
+			Center *t = aux->down; // se asigna un auxiliar hacia la sublista que ya tiene la caja principal que se le desea enlazar la otra sublista
+			while (t->next) // busca el final de la sublista de la caja principal
+				t = t->next;
+			t->next = (*c); //enlaza la ultima caja de la sublista, con el puntero de la sublita de la caja a eliminar
+			changeCodeCenter(&(t)->next, codeRNew);
+			break;
+		}
+		aux = aux->next;
+	}
+} // Solucionar, se interrumpe la corrida.
+
+void modifyRegister(Register **r)
+{
+	int op = -1; char name[65] = "", address[100] = "", codeR[4] = "", number[12] = "";
+	while (op != 0) {
+		system("cls");
+		printf("\n\n Indica lo que deseas modificar.\n\n");
+		printf("\n 1. Codigo de Region Estatal(Modifica Codigo Centros Electorales).");
+		printf("\n 2. Nombre.");
+		printf("\n 3. Direccion. ");
+		printf("\n 4. Numero.");
+		printf(" 0. Salir. \n\n");
+		printf("Indique opcion (0-4): ");
+		op = 0;
+		scanf("%i", &op);
+		system("cls");
+		printf("\n\n");
+		switch (op)
+		{
+		case 1: printf(" Indique Codigo Estatal: ");
+			scanf("%s", &codeR);
+			printf(" %s: " ,(*r)->code);
+			strcpy((*r)->code, codeR);
+			printf(" %s: ", (*r)->code);
+			changeCodeCenter(&(*r)->down, codeR);
+			break;
+
+		case 2:	printf(" Indique Nombre: ");
+			scanf("%s", &name);
+			strcpy((*r)->name, name);
+			break;
+
+		case 3:	printf(" Indique Direccion: ");
+			scanf("%s", &address);
+			strcpy((*r)->address, address);
+			break;
+
+		case 4:printf(" Indique Numero Telefonico: ");
+			scanf("%s", &number);
+			strcpy((*r)->number, number);
+			break;
+		}
+		if (op)
+		{
+			printf("\n\n");
+			system("pause");
+			system("cls");
+		}
+	}
+}
+
+void seachChangeDeleteRegister(Register **r, char codeR[4], bool flag) 
+{
+	Register *aux = (*r);
+	if (!(vacio(aux)))
+	{
+		if ((strcmp(codeR, aux->code) == 0)) //caso cabeza
+		{
+			(*r) = (*r)->next;
+			if (flag)
+				modifyRegister(&(*r));
+			else // else if (!flag && !vacio(t->down))
+			{
+				changeRegister(r, &(*r)->down, codeR);
+				delete aux;
+			}
+		}
+		else
+		{
+			while (aux->next)
+			{
+				if ((strcmp(codeR, aux->next->code) == 0))
+				{
+					Register *t = aux->next;
+					aux->next = aux->next->next;
+					if (flag)
+						modifyRegister(&t);
+					else // else if (!flag && !vacio(t->down))
+					{
+						changeRegister(r, &(t)->down, codeR);
+						delete t;
+					}
+					break;
+				}
+				aux = aux->next;
+			}
+		}
+	}
+} // falta el caso de si es el cabeza de a lista. 
