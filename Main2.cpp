50c391567daf9f949ecbdddeba6cdc477ac9a36f
Ruben Luna <rubenlunamate@gmail.com>
2015-12-15 21:25:51 -0430

    Update Main2.cpp

--- Main2.cpp
+++ Main2.cpp
@@ -1,5 +1,3 @@
-# RegistroElectoral
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -7,9 +5,11 @@
 #include "Center.h"
 #include "Register.h"
 
+
+
 void main()
 {
-	int op = -1; char ID[10], name[65], address[100], date[9], codeC[8], codeR[4], number[12];
+	int op = -1; char ID[10] = "", name[65] = "", address[100] = "", date[9] = "", codeC[8] = "", codeR[4] = "", number[12] = "";
 	Register *r = NULL;
 	Center *c = NULL;
 	People *p = NULL;
@@ -20,7 +20,6 @@ void main()
 		printf("\n 2. Insertar Centro Electoral en Registrol Estatal.");
 		printf("\n 3. Ingresar Persona en Centro Electoral. ");
 		printf("\n 4. Mostrar todo. \n\n");
-
 		printf(" 0. Salir. \n\n");
 		printf("Indique opcion (1-5): ");
 		op = 0;
@@ -30,37 +29,31 @@ void main()
 		bool flag = true;
 		switch (op)
 		{
-			case 1: printf(" Indique datos: ");
-				scanf("%s", &codeR);
-				scanf("%s", &name);
-				scanf("%s", &number);
-				scanf("%s", &address);
-				addRegister(&r, name, address, number, codeR);
-				break;
+		case 1: printf(" Indique datos: ");
+			scanf("%s", &codeR);
+			scanf("%s", &name);
+			scanf("%s", &number);
+			scanf("%s", &address);
+			addRegister(&r, name, address, number, codeR);
+			break;
 
-			case 2:	printf(" Indique codigo Registro Estatal: ");
-					scanf("%s", &codeR);
-					addCenterRegister(r, codeR);
-				break;
+		case 2:	printf(" Indique codigo Registro Estatal: ");
+			scanf("%s", &codeR);
+			addCenterRegister(r, codeR);
+			break;
 
-			case 3:	printf(" Indique codigo centro electoral: "); 
-					scanf("%s", &codeC);
-					// Buscar en que registro estatal esta ese centro electoral, para agragar la persona
-					addPersonCenter(c, codeC);
-				/* printf(" Indique sus cedula: ");
-				scanf("%s", &ID);
-				scanf("%s", &name);
-				scanf("%s", &address);
-				scanf("%s", &date);
-				addPerson(&p, ID, name, address, date); */
-				break;
+		case 3:	printf(" Indique codigo centro electoral: ");
+			scanf("%s", &codeC);
+			// Buscar en que registro estatal esta ese centro electoral, para agragar la persona
+			seachCenterRegister(r, codeC);
+			break;
 
-			case 4:
-				printf(" Centros electorales con personas: \n");
-				seeAll(r);
-				break;
+		case 4:
+			printf(" Centros electorales con personas: \n");
+			seeAll(r);
+			break;
 
-			 
+
 		}
 		if (op)
 		{
@@ -70,4 +63,3 @@ void main()
 		}
 	}
 }
-
