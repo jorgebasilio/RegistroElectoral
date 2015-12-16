070e59188e25a9f4af358b9db4b237fc4f583510
Ruben Luna <rubenlunamate@gmail.com>
2015-12-15 23:06:32 -0430

    Update Center.h

--- Center.h
+++ Center.h
@@ -125,4 +125,44 @@ void seePersonCenter(Center *c)
 		}
 	}
 }
+void change(Center **c, People **p, char codeC[8]) // parametros: lista principal completa, sub-lita a enlazar, numero X que se va a eliminar de la principal 
+{
+	char codeCNew[8];
+	printf("\n Codigo Centro Electoral donde iran los electores :");
+	scanf("%s", &codeCNew);
+	Center *aux = (*c);
+	while (aux)
+	{
+		if ((strcmp(codeCNew, aux->code) == 0) && (strcmp(codeCNew, codeC) != 0)) // se busca la caja principal a enlazar con sublista de X, mientras sea distinto de X
+		{
+			People *t = aux->left; // se asigna un auxiliar hacia la sublista que ya tiene la caja principal que se le desea enlazar la otra sublista
+			while (t->next) // busca el final de la sublista de la caja principal
+				t = t->next;
+			t->next = (*p); //enlaza la ultima caja de la sublista, con el puntero de la sublita de la caja a eliminar
+			break;
+		}
+		aux = aux->next;
+	}
+}
+
+void seachChangeDelete(Center **c, char codeC[8], bool flag)
+{
+	Center *aux = (*c);
+	while (aux)
+	{
+		printf("\n Entro while seachChangeDelete");
+		if ((strcmp(codeC, aux->next->code) == 0)) // se evalua para buscar el numero a eliminar
+		{
+			Center *t = aux->next;
+			aux->next = aux->next->next;
+			if (flag && !vacio(t->left)) // depende de la opcion del usuario, de lo que desea hacer
+				DeletePeople(&(t)->left); // elimina toda la sub-lista
+			else if (!flag && !vacio(t->left))
+				change(c, &(t)->left, codeC); // busca otra caja principal para poner la sub-lista de la caja a eliminar
+			delete t;
+			break;
+		}
+		aux = aux->next;
+	}
+}
 
